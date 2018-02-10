872310e2eeee3c8743ad78fb87873ffc37d3a619
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-02-10 10:15:11 -0500

    SImple fizzbuzz ver1.
    JS practice 1

---
+++ fizzbuzz_ver1.js
@@ -0,0 +1,14 @@
+// This is a basic first version of fizzbuzz using JS.
+
+var val = prompt("Enter your value");
+
+console.log("The input value is:"+val);
+
+if(val%3 === 0)
+{
+    console.log("print fizz");
+}
+else if(val%5 === 0)
+{
+    console.log("Print buzz");
+}
